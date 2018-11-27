9856fc2d2d022cf1863b38d6bb939762fb8c764d
Tony Gjendahl <tony.gjendahl@nav.no>
2018-11-27 11:23:50 +0100

    Legge til header for personbruker

--- pam-frontend-header/src/index.tsx
+++ pam-frontend-header/src/index.tsx
@@ -2,5 +2,9 @@ import { ArbeidsgiverHeaderMeny, ArbeidsgiverTabId } from './arbeidsgiver/Header
 import { ArbeidsgiverHeader } from './arbeidsgiver/Header';
 import { VelgArbeidsgiver } from './arbeidsgiver/VelgArbeidsgiver';
 import { VeilederHeaderMeny, VeilederTabId } from './veileder/HeaderMeny';
+import { PersonbrukerHeaderMeny } from "./personbruker/PersonbrukerHeaderMeny";
+import { PersonbrukerHeader } from './personbruker/PersonbrukerHeader';
 
-export { ArbeidsgiverHeader, ArbeidsgiverHeaderMeny, ArbeidsgiverTabId, VelgArbeidsgiver, VeilederHeaderMeny, VeilederTabId }
+export { ArbeidsgiverHeader, ArbeidsgiverHeaderMeny, ArbeidsgiverTabId, VelgArbeidsgiver, VeilederHeaderMeny,
+    VeilederTabId, PersonbrukerHeaderMeny, PersonbrukerHeader }
+
---
+++ pam-frontend-header/src/personbruker/PersonbrukerHeader.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './PersonbrukerHeaderMeny.less';
+
+export const PersonbrukerHeader = () => (
+  <div className="HeaderMeny">
+    <div className="topp">
+      <div className="logo">
+        <a href="/">Arbeidsplassen</a>
+      </div>
+    </div>
+  </div>
+);
