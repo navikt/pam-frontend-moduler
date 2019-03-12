c3f4c66ecef1a93f6631613864c16aec8774f6a4
Tony Gjendahl <tony.gjendahl@nav.no>
2019-03-12 14:21:56 +0100

    PAM-2852 Legge til mulighet for å sende med funksjon for å logge ut

--- pam-frontend-header/src/personbruker/HeaderUtenMenypunkter.tsx
+++ pam-frontend-header/src/personbruker/HeaderUtenMenypunkter.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
 
 interface HeaderUtenMenypunkterProps {
-    onLoggInn: () => void;
+    onLoggInn?: () => void;
+    onLoggUt?: () => void;
     erLoggetInn: boolean;
 }
 
-export const HeaderUtenMenypunkter = ({ onLoggInn, erLoggetInn } : HeaderUtenMenypunkterProps ) => (
+export const HeaderUtenMenypunkter = ({ onLoggInn, onLoggUt, erLoggetInn } : HeaderUtenMenypunkterProps ) => (
     <div className="HeaderUtenMenypunkter">
         <div className="topp">
             <div className="logo">
                 <a className="lenke" href="/">Arbeidsplassen</a>
             </div>
-            <button onClick={onLoggInn} id="logg-inn" className="Button Button--mini typo-normal">
-                {erLoggetInn ? 'Logg ut' : 'Logg inn'}
-            </button>
+            {erLoggetInn ? (
+                <button onClick={onLoggUt} id="logg-inn" className="Button Button--mini typo-normal">
+                    Logg ut
+                </button>
+            ) : (
+                <button onClick={onLoggInn} id="logg-inn" className="Button Button--mini typo-normal">
+                    Logg inn
+                </button>
+            )}
         </div>
     </div>
 );
