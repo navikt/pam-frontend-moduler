ff00c80c4eda0c5ab3d6e54bbc64250f4ecd9853
Tony Gjendahl <tony.gjendahl@nav.no>
2019-03-13 14:18:58 +0100

    PAM-2889 Legge til pam-frontend-footer som en egen modul

---
+++ pam-frontend-footer/src/footer/Footer.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.less'
+
+interface InternalOrExternalLinkProps {
+  href: string;
+  isInternalLink: boolean;
+  children: string;
+}
+
+const InternalOrExternalLink = ({ href, isInternalLink, children }: InternalOrExternalLinkProps) => {
+    if (isInternalLink) {
+        return (
+            <Link to={href} className="Footer__links__ul__li__link">
+                {children}
+            </Link>
+        );
+    }
+    return (
+        <a href={href} className="Footer__links__ul__li__link">
+            {children}
+        </a>
+    );
+};
+
+interface FooterProps {
+  useInternalLinks?: boolean;
+}
+
+
+const Footer = ({ useInternalLinks = false }: FooterProps) => (
+  <div className="Footer">
+    <div className="Footer__inner">
+      <p className="Footer__slogan">Alt av arbeid på ett sted.</p>
+      <nav className="Footer__links">
+        <a className="Footer__logo lenke" href="https://www.nav.no/Forsiden" title="Gå til nav.no">
+          <i className="Footer__logo__nav-icon" aria-label="NAV logo" />
+        </a>
+        <ul className="Footer__links__ul">
+          <li className="Footer__links__ul__li">
+            <InternalOrExternalLink isInternalLink={useInternalLinks} href="/om-arbeidsplassen">
+              Om arbeidsplassen
+            </InternalOrExternalLink>
+          </li>
+          <li className="Footer__links__ul__li">
+            <InternalOrExternalLink isInternalLink={useInternalLinks} href="/kontaktinfo">
+              Kontaktinfo
+            </InternalOrExternalLink>
+          </li>
+          <li className="Footer__links__ul__li">
+            <InternalOrExternalLink isInternalLink={useInternalLinks} href="/tilgjengelighet">
+              Tilgjengelighet
+            </InternalOrExternalLink>
+          </li>
+          <li className="Footer__links__ul__li">
+            <InternalOrExternalLink isInternalLink={useInternalLinks} href="/personvern">
+              Personvern
+            </InternalOrExternalLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+);
+
+export default Footer;
